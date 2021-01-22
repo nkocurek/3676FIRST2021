ab691c30f98e3f33b21e0711abe4a7ce11f6ae4c
Warrior Robotics <47011895+WarriorRobotics3676@users.noreply.github.com>
2021-01-22 14:29:25 -0600

    test

--- src/main/java/frc/robot/commands/DriveForDist.java
+++ src/main/java/frc/robot/commands/DriveForDist.java
@@ -7,7 +7,6 @@
 
 package frc.robot.commands;
 
-import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.SpeedConstants;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.CommandBase;
--- src/main/java/frc/robot/subsystems/BetterElevator.java
+++ src/main/java/frc/robot/subsystems/BetterElevator.java
@@ -10,13 +10,8 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.RobotContainer;
-import frc.robot.Constants.ElevatorConstants;
 import frc.robot.Constants.PortConstants;
 import frc.robot.Constants.SpeedConstants;
 
